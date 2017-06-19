f004d2a03d9c0a17dcc20a1508fd3411fc2d41ab
horasikus <hglagan@gmail.com>
2017-06-19 12:22:33 +0200

    make it better

--- src/services/media.js
+++ src/services/media.js
@@ -4,13 +4,22 @@ var Promise = require('bluebird');
 var logger = require('./../../config/logger');
 var fs = require('fs');
 var youtubedl = require('youtube-dl');
-var os = require('os');
 var path = require('path');
 var crypto = require('crypto');
 
+var DEFAULT_OPTIONS = {
+    output: 'data/media'
+}
+
 exports.getMediaAsync = function (name, url) {
 
-    var tmpFile = path.resolve(os.tmpdir(), crypto.createHash('md5').update(name).digest('hex'));
+    var mediaDir = path.resolve(process.cwd(), DEFAULT_OPTIONS.output);
+
+    if (!fs.existsSync(mediaDir)) {
+        fs.mkdirSync(mediaDir);
+    }
+
+    var mediaFile = path.resolve(mediaDir, crypto.createHash('md5').update(name).digest('hex'));
 
     return new Promise(function (resolve, reject) {
 
@@ -22,20 +31,20 @@ exports.getMediaAsync = function (name, url) {
 
         // Will be called when the download starts.
         video.on('info', function (info) {
-            logger.info(`Youtube: Downloading ${info._filename} size ${info.size}`);
+            logger.info(`Media service: downloading ${info._filename} size ${info.size}`);
         });
 
         // Will be called if download was already completed and there is nothing more to download.
         video.on('complete', function complete(info) {
-            logger.info(`Youtube: filename ${info._filename} already downloaded.`);
+            logger.info(`Media service: filename ${info._filename} already downloaded.`);
         });
 
         // HORACIO_TODO: filename path
-        video.pipe(fs.createWriteStream(tmpFile));
+        video.pipe(fs.createWriteStream(mediaFile));
 
         video.on('end', function () {
-            logger.info(`Youtube: Downloaded ${tmpFile}`);
-            return resolve(tmpFile);
+            logger.info(`Media service: Downloaded ${mediaFile}`);
+            return resolve(mediaFile);
         });
 
         video.on('error', function error(err) {
